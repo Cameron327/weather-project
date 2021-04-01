35584e89f3bacc5929bd6a70042a4d2a1df5720e
Cameron <yeec327@gmail.com>
2021-03-31 23:04:33 -0700

    Properly parsed JSON object using the .on method to access the data

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const app = express();
 app.get('/', function(req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Davis&appid=4d7a6e6ac08edf9e8d2dcd2712895d90";
     https.get(url, function(response) {
-        console.log(response);
+        console.log(response.statusCode);
+
+        // this function is used to basically unpack the JSON object that we got and putting it back into a readable JSON object using .parse
+        response.on("data", function(data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+        });
     });
 
 
