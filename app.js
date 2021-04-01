b8331ff3de6da7138d31d558c04d1c8edd98317a
Cameron <yeec327@gmail.com>
2021-03-31 23:31:34 -0700

    Implemented the data from the JSON to display what the live weather is for Davis, CA, including an image of the weather

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Davis&appid=4d7a6e6ac08edf9e8d2dcd2712895d90";
     https.get(url, function(response) {
         console.log(response.statusCode);
+        console.log(response);
 
         // this function is used to basically unpack the JSON object that we got and putting it back into a readable JSON object using .parse
         response.on("data", function(data) {
@@ -16,11 +17,16 @@ app.get('/', function(req, res) {
             console.log(weatherData);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+            const imageUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+
+            res.write("<p>The weather is currently " + weatherDescription + "</p>")
+            res.write("<h1>The temp in Davis, CA is " + temp + " Kelvin.</h1>");
+            res.write("<img src=" + imageUrl + ">")
+            res.send();
         });
     });
 
-
-    res.send("Server is running");
 });
 
 
